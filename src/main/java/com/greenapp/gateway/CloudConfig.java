71df0028677a12df6b01a099b4d71c87adc429b0
boriskezikov <boris200898@icloud.com>
2020-05-28 02:32:28 +0300

    #Basic auth experiment #dev props added

--- src/main/java/com/greenapp/gateway/CloudConfig.java
+++ src/main/java/com/greenapp/gateway/CloudConfig.java
@@ -7,16 +7,17 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.gateway.route.builder.UriSpec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
+import java.util.logging.Logger;
 
 @Configuration
 public class CloudConfig {
+
+    private static final Logger LOG = Logger.getLogger(CloudConfig.class.getName());
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
@@ -39,15 +40,15 @@ public class CloudConfig {
     }
 
     private Mono<Void> addHeaders(ServerWebExchange exchange, GatewayFilterChain chain) {
-        MultiValueMap<String, String> map = new HttpHeaders();
-        map.add("myNewHeader", "value");
-        exchange.getResponse()
-                .getHeaders()
-                .addAll(map);
 
         ServerHttpRequest request = exchange.getRequest().mutate()
-                .header("test-header", "123456")
+                .header("X-GREEN-APP-ID", "GREEN")
+                .headers(httpHeaders -> httpHeaders.setBasicAuth("green", "greenapp"))
                 .build();
+
+        LOG.info(String.format("Accepting request: %s", request.getURI()));
+        LOG.info(String.format("Headers: %s", request.getHeaders()));
+        LOG.info(String.format("Cookies: %s", request.getCookies()));
 
         exchange.mutate()
                 .request(request)
