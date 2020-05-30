d998e49968c6b3c50413dcd1858b07436c12ae7b
boriskezikov <boris200898@icloud.com>
2020-05-30 05:42:03 +0300

    bunners

--- src/main/java/com/greenapp/gateway/configuration/ExceptionHandlerInterceptor.java
+++ src/main/java/com/greenapp/gateway/configuration/ExceptionHandlerInterceptor.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.net.SocketTimeoutException;
 
 @ControllerAdvice
 public class ExceptionHandlerInterceptor extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = ZuulException.class)
+    @ExceptionHandler(value = {ZuulException.class, SocketTimeoutException.class})
     protected ResponseEntity<Object> handle(Exception ex, WebRequest request) {
         throw new ServiceUnavailableException("Sorry, heroku issues:)");
     }
